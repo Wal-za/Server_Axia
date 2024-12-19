886e526b98cd0f43b76387cb7e1acadbeb4b263a
Waltza1 <111610017+Waltza1@users.noreply.github.com>
2024-12-19 13:02:56 -0500

    sesion terminada

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,9 @@
 const ClienteFormulario = require('../models/ClienteAxia'); 
-const bcrypt = require('bcryptjs'); 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); 
 
-// Controlador para login (iniciar sesión)
+const SECRET_KEY = 'dgfghdgfdslkjhcsdg54v1df2g5vdf1vd152fdg13f5v4hb1fg2b65g1nb53fg13b5gf1vb53f1gv'; 
+
 const loginCliente = async (req, res) => {
   try {
     const { username, password } = req.body; 
@@ -9,21 +11,28 @@ const loginCliente = async (req, res) => {
     const cliente = await ClienteFormulario.findOne({ correoElectronico: username }); 
     
     if (!cliente) {
-      return res.status(400).json({ message: 'Correo electrónico no registrado' });
+      return res.status(400).json({ valid: false, message: 'Usuario y contraseña incorrectos' });
     }
 
     const esContraseñaValida = await bcrypt.compare(password, cliente.contraseña); 
     
     if (!esContraseñaValida) {
-      return res.status(400).json({ message: 'Contraseña incorrecta' });
+      return res.status(400).json({ valid: false, message: 'Usuario y contraseña incorrectos' });
     }
-
+   
+    const token = jwt.sign(
+      { id: cliente._id, correoElectronico: cliente.correoElectronico }, 
+      SECRET_KEY, 
+      { expiresIn: '1h' } 
+    );
+   
     res.status(200).json({
       valid: true, 
-      cliente: cliente // Enviar todos los datos del cliente al front
+      cedula: cliente.cedula,  
+      token: token       
     });
   } catch (error) {
-    res.status(500).json({ message: 'Error al iniciar sesión', error: error.message });
+    res.status(500).json({ valid: false, message: 'Error al iniciar sesión', error: error.message });
   }
 };
 
