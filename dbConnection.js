789ba7343ac1ae57f662f21a614a5687781332b5
Waltza1 <111610017+Waltza1@users.noreply.github.com>
2025-02-18 13:19:57 -0500

    mongo original

--- dbConnection.js
+++ dbConnection.js
@@ -1,10 +1,8 @@
 const mongoose=require('mongoose')
 mongoose.set('strictQuery', false);
-const password ='1234'
+const password ='12345'
 
-//const connectionString=`mongodb+srv://daniel94cruz:${password}@cluster0.ecmhoaq.mongodb.net/Axia?retryWrites=true&w=majority&appName=Cluster0`
-const connectionString=`mongodb+srv://AXIAFINANZAS:${password}@cluster0.rofpd.mongodb.net/Axia?retryWrites=true&w=majority&appName=Cluster0`
-
+const connectionString=`mongodb+srv://daniel94cruz:${password}@cluster0.ecmhoaq.mongodb.net/Axia?retryWrites=true&w=majority&appName=Cluster0`
 const connectDB=async() => {
    await mongoose.connect(connectionString)
 .then(() => {
