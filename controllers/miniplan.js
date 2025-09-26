514270d9117457549bcb3c8077114cf3214bfb6c
danielcruz94 <daniel94cruz@gmail.com>
2025-09-25 21:31:57 -0500

    fix:enviarCorreoSinPDF

--- controllers/miniplan.js
+++ controllers/miniplan.js
@@ -130,7 +130,7 @@ const procesarMiniPlan = async (req, res) => {
         doc.on('end', async () => {
             const pdfData = Buffer.concat(buffers);
 
-             enviarCorreoConPDF(datosPlan, pdfData);
+            enviarCorreoSinPDF(datosPlan, pdfData);
             
 
             res.set({
