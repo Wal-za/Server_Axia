85a1f926d6618cade3338309941c4eb376ee149e
Waltza1 <111610017+Waltza1@users.noreply.github.com>
2025-10-24 19:52:58 -0500

    pruebas mongo bd

--- routes/clienteAxiaRoutes.js
+++ routes/clienteAxiaRoutes.js
@@ -9,7 +9,13 @@ const obtenerFieldset = require('../controllers/fieldset');
 const getAllClientes = require('../controllers/GetAllClientes');
 const { procesarMiniPlan } = require('../controllers/miniplan');
 const { exportarClientes } = require('../controllers/exportarClientesController');
+const { enviarCorreoConPDF } = require('../controllers/enviarCorreo');
 
+const multer = require('multer');
+
+// Configurar multer para manejar el archivo PDF
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // Ruta para crear un nuevo cliente
 router.post('/clientes', crearCliente);
@@ -33,10 +39,32 @@ router.post('/miniplan', procesarMiniPlan);
 //Ruta Mini Plan Financiero Descargar datos
 router.get('/ClienteAxias', exportarClientes);
 
+// Ruta para enviar el correo con el PDF adjunto
+router.post('/Email', upload.single('pdf'), async (req, res) => {
+    try {
+
+        if (!req.file || !req.file.buffer) {
+            return res.status(400).json({ message: 'No se recibió el archivo PDF' });
+        }
+
+        // Parsear el JSON que viene como string
+        const parsedData = JSON.parse(req.body.object);
+        const { nombre, email, celular, recomendadoPor } = parsedData;
+
+        const pdfBuffer = req.file.buffer;
+
+        await enviarCorreoConPDF({ nombre, email, celular, recomendadoPor }, pdfBuffer);
+        res.status(200).json({ message: 'Correo enviado correctamente' });
+    } catch (error) {
+        console.error("❌ Error al enviar el correo:", error);
+        res.status(500).json({ message: 'Error al enviar el correo' });
+    }
+});
+
 
 // ✅ Ruta keepalive para mantener el servidor activo
 router.get('/keepalive', (req, res) => {
-  res.status(200).send('✅ Server is awake!');
+    res.status(200).send('✅ Server is awake!');
 });
 
 module.exports = router;
