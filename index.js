07f59e599cc450b112a97d0873ae110c4b6a8a7f
Waltza1 <111610017+Waltza1@users.noreply.github.com>
2025-11-11 13:18:51 -0500

    prueba 2 error

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const cors = require('cors');
 require('dotenv').config();
 const path = require('path');
 
+
 const connectDB = require('./dbConnection'); 
 const clienteRoutes = require('./routes/clienteAxiaRoutes'); 
 
 const app = express();
 
-app.use(cors({
-  origin: "*", 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-app.options('*', cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); 
+  }
+  next();
+});
 
 app.use(express.json());
 
@@ -26,6 +30,7 @@ app.get('/', (req, res) => {
 });
 
 if (process.env.NODE_ENV !== 'production') {
+  // Solo escucha en local
   const port = process.env.PORT || 3001;
   app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
@@ -35,5 +40,5 @@ if (process.env.NODE_ENV !== 'production') {
 app.get('/favicon.png', (req, res) => {
   res.sendFile(path.join(__dirname, 'favicon.png'));
 });
-
+// Exporta siempre la app para Vercel
 module.exports = app;
